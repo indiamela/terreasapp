919d1da2866b40309c06bed377d3d81347f89a2b
indiamela <taishi.kusunose@gmail.com>
2020-05-20 12:38:32 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jikancafe, only: [:index]
-  resources :top, only: [:index]
 
 end
