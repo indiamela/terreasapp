c9b0c90df86d18198b2d71c54c806f5f5558721f
indiamela <taishi.kusunose@gmail.com>
2020-06-02 09:36:17 +0900

    css

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,6 +4,16 @@ class TopController < ApplicationController
     @manuals = Manual.all
   end
   def new
-    
+  end
+  def create
+    Manual.create(title: manual_params[:title],picture: manual_params[:picture],content: manual_params[:content], category_id: current_category.id)
+  end
+
+  private
+  def category_params
+    params.permit(:name)
+  end
+  def manual_params
+    params.permit(:title,:picture,:content)
   end
 end
